fcb755a33aade333a7343ec99fbee714abd067ec
Jeremy Adams <adamsj@objectcomputing.com>
2020-02-20 17:28:27 -0600

    switch script to bash to work inside container

--- create_workspace.sh
+++ create_workspace.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env zsh
-script_path=${0:a:h}
+#!/usr/bin/env bash
+script=`realpath $0`
+script_path=`dirname $script`
 pushd $script_path
 pushd ..
 if [ ! -d "src" ];then
     echo "no src"
     mkdir src
-
 fi
 vcs import src < rmw_build/ros2.repos
 popd
