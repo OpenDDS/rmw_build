c7d0af160f9924be49ef20f3ea5b07bce6b92d83
Jeremy Adams <adamsj@objectcomputing.com>
2020-02-25 16:05:45 -0600

    build with symbols and verbose make
    help with debugging during development

--- build_rmw.sh
+++ build_rmw.sh
@@ -4,6 +4,6 @@ script_path=`dirname $script`
 pushd $script_path
 pushd ..
 . /opt/ros/eloquent/setup.bash
-colcon build --symlink-install --packages-up-to rmw_opendds_cpp
+colcon build --symlink-install --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rmw_opendds_cpp
 popd
 popd
