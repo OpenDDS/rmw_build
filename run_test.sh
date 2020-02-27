9261231d70d779ed1aaefbd03fae091d3551cedb
Jeremy Adams <adamsj@objectcomputing.com>
2020-02-27 09:34:39 -0600

    gdb runs at start or optionally on command
    -b option allows to set breakpoints or interact with the runtime

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 alt_rmw="rmw_opendds_cpp"
 run_test="examples_rclcpp_minimal_publisher publisher_member_function"
-while getopts ":ha:s" opt; do
+gdb_cmd="gdb -ex run --args"
+
+while getopts ":ha:sb" opt; do
 case ${opt} in
     a ) 
         alt_rmw=$OPTARG
@@ -10,15 +12,16 @@ case ${opt} in
     s ) 
         run_test="examples_rclcpp_minimal_subscriber subscriber_member_function"
     ;;    
-    h ) echo "options: [-s] switch to subscriber [-a] use alternate rmw (i.e. rmw_cyclonedds_cpp, rmw_fastrtps_cpp, or rmw_connext_cpp). For optional DDS implementations, only rmw_fastrtps_cpp is installed by default. All others require extra installation steps. https://index.ros.org/doc/ros2/Installation/DDS-Implementations/"
+    b )
+        gdb_cmd="gdb --args"
+    ;;
+    h ) echo "options: [-s] switch to subscriber [-a] use alternate rmw (i.e. rmw_cyclonedds_cpp, rmw_fastrtps_cpp, or rmw_connext_cpp). [-b] breakpoint debugging"
     exit
     ;;
 esac
 done
-echo $alt_rmw
-echo $run_test
  
-which gdb
+which gdb &> /dev/null
 if [ $? == 1 ];then
     apt update
     apt install -y gdb
@@ -26,11 +29,11 @@ fi
 
 script=`realpath $0`
 script_path=`dirname $script`
-pushd $script_path
-pushd ..
+pushd $script_path &> /dev/null
+pushd .. &> /dev/null
 . /opt/ros/eloquent/setup.bash
 . install/local_setup.bash
-RMW_IMPLEMENTATION=$alt_rmw ros2 run --prefix 'gdb -ex run --args' $run_test
-popd 
-popd
+eval RMW_IMPLEMENTATION="$alt_rmw ros2 run --prefix '${gdb_cmd}' $run_test"
+popd &> /dev/null
+popd &> /dev/null
  
