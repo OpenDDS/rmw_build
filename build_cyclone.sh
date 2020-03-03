8fd8f1f4cd776bdbaaae434d5695020900224f2e
Clayton Calabrese <calabreseclayton@gmail.com>
2020-03-03 15:53:52 -0600

    Installing cyclonedds now

--- build_cyclone.sh
+++ build_cyclone.sh
@@ -4,13 +4,8 @@ script_path=`dirname $script`
 pushd $script_path
 pushd ..
 . /opt/ros/eloquent/setup.bash
-
-#dpkg-query -W -f='${Status}\n' ros-eloquent-cyclonedds &> /dev/null
-#if [ ${Status} == "install ok installed" ];then
-    apt update
-    apt install -y ros-eloquent-cyclonedds
-#fi
-
+apt update
+apt install -y ros-eloquent-cyclonedds
 colcon build --symlink-install --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rmw_cyclonedds_cpp
 popd
 popd
