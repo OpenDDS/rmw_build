0de0a7f74c7304a408beaf6206e54a371d78bd02
Jeremy Adams <adamsj@objectcomputing.com>
2020-03-18 10:00:27 -0500

    add system_test repo and other cleanup

--- build_cyclone.sh
+++ build_cyclone.sh
@@ -4,8 +4,11 @@ script_path=`dirname $script`
 pushd $script_path
 pushd ..
 . /opt/ros/eloquent/setup.bash
-apt update
-apt install -y ros-eloquent-cyclonedds
+dpkg -l ros-eloquent-cyclonedds &>/dev/null
+if [ $? == 1 ];then
+    apt update
+    apt install -y ros-eloquent-cyclonedds
+fi
 colcon build --symlink-install --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rmw_cyclonedds_cpp
 popd
 popd
