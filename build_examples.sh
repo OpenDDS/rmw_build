db219f319476979c41888dcee7a8795b22d79916
Jeremy Adams <adamsj@objectcomputing.com>
2020-03-19 12:03:26 -0500

    push and pop fixes

--- build_examples.sh
+++ build_examples.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 script=`realpath $0`
 script_path=`dirname $script`
-pushd $script_path
-pushd ..
+pushd $script_path &> /dev/null
+pushd .. &> /dev/null
 . /opt/ros/eloquent/setup.bash
 colcon build --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to examples_rclcpp_minimal_publisher
 colcon build --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to examples_rclcpp_minimal_subscriber
-popd
-pop
+popd &> /dev/null
+popd &> /dev/null
