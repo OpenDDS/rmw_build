db948b760e1d2c6143c9854cbc2dc661718e6f41
Jeremy Adams <adamsj@objectcomputing.com>
2020-04-28 14:17:49 -0500

    separate rmw from depends for use in CI

--- create_workspace.sh
+++ create_workspace.sh
@@ -3,10 +3,13 @@ script=`realpath $0`
 script_path=`dirname $script`
 pushd $script_path &> /dev/null
 pushd .. &> /dev/null
+
 if [ ! -d "src" ];then
     echo "no src"
     mkdir src
 fi
 vcs import src < rmw_build/ros2.repos
+vcs import src < rmw_build/rmw_opendds.repos
+
 popd &> /dev/null
 popd &> /dev/null
