73ad40896d6b683095ac863327564c52b213fd31
Jeremy Adams <adamsj@objectcomputing.com>
2020-04-28 12:55:58 -0500

    helper to perform various git commands
    - TODO: investigate performance of find operation in container

---
+++ all_git_repos.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+TXTGRN=$(tput setaf 2) # Green
+TXTRST=$(tput sgr0) # Text reset.
+
+script_path=`dirname $0`
+
+command=""
+pipe=""
+while getopts ":spbSHh" opt; do
+case ${opt} in 
+    s )
+        command="status -s"
+    ;;
+    p )
+        command="remote -v"
+    ;;
+    b )
+        command="branch -a"
+    ;;
+    S )
+        command="show -s"
+    ;;
+    H )
+        command="show -s"
+        pipe="|grep -B 1 commit"
+    ;;
+    h )
+        echo "options: [-s] (status) [-p] (paths) [-b] (branches) [-S] (show) [-H] (show hashes)"
+        exit
+    ;;
+esac
+done
+
+pushd $script_path &> /dev/null
+pushd .. &> /dev/null
+
+findgit=$(find . -name ".git")
+for x in $findgit; do
+    pushd $(dirname $x) &>/dev/null
+    echo -e "${TXTGRN}$(basename $(dirname $x))${TXTRST}"
+    eval git $command$pipe
+    popd &>/dev/null
+done
+
+popd &> /dev/null
+popd &> /dev/null
