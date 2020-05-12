221fc647bcb26d2f284f53cec38fd1da7862f7ab
Jeremy Adams <adamsj@objectcomputing.com>
2020-05-12 13:10:43 -0500

    optionally build RPC (client/server) and new CI test

--- build_all.sh
+++ build_all.sh
@@ -3,12 +3,16 @@ script=`realpath $0`
 script_path=`dirname $script`
 
 build_cyclonedds="no"
-while getopts ":hc" opt; do
+build_rpc=""
+while getopts ":hcr" opt; do
 case ${opt} in
     c ) 
         build_cyclonedds="yes"
     ;;  
-    h ) echo "options: [-c] build cyclone"
+    r ) 
+        build_rpc="examples_rclcpp_minimal_service examples_rclcpp_minimal_client"
+    ;;  
+    h ) echo "options: [-c] build cyclone [-r] build examples for RPC"
     exit
     ;;
 esac
@@ -28,7 +32,7 @@ pushd .. &> /dev/null
 echo "BUILD ALL"
 colcon build --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rmw_opendds_cpp  
 . install/local_setup.bash
-colcon build --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rcl_interfaces examples_rclcpp_minimal_publisher examples_rclcpp_minimal_subscriber 
+colcon build --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rcl_interfaces examples_rclcpp_minimal_publisher examples_rclcpp_minimal_subscriber $build_rpc
 
 popd &> /dev/null
 popd &> /dev/null
---
+++ run_ci_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+source /opt/OpenDDS/setenv.sh
+source /opt/ros/eloquent/setup.bash
+source install/local_setup.bash
+printenv|grep DDS
+printenv|grep ROS
+
+export RMW_IMPLEMENTATION=rmw_opendds_cpp
+printenv|grep RMW
+
+ros2 run examples_rclcpp_minimal_subscriber subscriber_member_function_twenty 1> sub.out& 
+process_id=$!
+ros2 run examples_rclcpp_minimal_publisher publisher_member_function_twenty
+wait $process_id
+cat sub.out
+grep "I heard" sub.out
+ 
