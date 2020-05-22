3b9ac4ff63bfe3627e09acd00050253bd23ffeec
Jeremy Adams <adamsj@objectcomputing.com>
2020-05-22 15:28:54 -0500

    remove unique install base locations

--- build_system_test.sh
+++ build_system_test.sh
@@ -21,28 +21,20 @@ done
 pushd $script_path &> /dev/null
 pushd .. &> /dev/null
 
-while true; do
-    read -p "Remove build directory to prevent build contanimation?" yn
-    case $yn in
-        [Yy]* ) rm -rf build; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
 . /opt/ros/eloquent/setup.bash
 echo $use
 if [ $use == "opendds" ];then
-    ./rmw_build/build_rmw.sh -i install_test_$use
+    ./rmw_build/build_rmw.sh
 fi
 if [ $use == "cyclone" ];then
-    ./rmw_build/build_cyclone.sh -i install_test_$use
+    ./rmw_build/build_cyclone.sh
 fi
 if [ $use != "fastrtps" ];then
-    . install_test_$use/local_setup.bash
+    . install/local_setup.bash
 fi
 
-colcon build --install-base install_test_$use --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rcl_interfaces test_communication
+
+colcon build --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rcl_interfaces test_communication
 
 popd &> /dev/null
 popd &> /dev/null
