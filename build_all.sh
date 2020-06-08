20a426852242e8bd49635c8d9a87d13ee796c60e
Jeremy Adams <adamsj@objectcomputing.com>
2020-06-08 12:00:40 -0500

    make build script show failures in CI

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-script=`realpath $0`
-script_path=`dirname $script`
 
 build_rpc=""
 while getopts ":hr" opt; do
@@ -17,12 +15,7 @@ done
 export PATH=/usr/lib/ccache:$PATH
 . /opt/ros/foxy/setup.bash
 
-pushd $script_path  &> /dev/null
-pushd .. &> /dev/null
 echo "BUILD ALL"
 colcon build --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rmw_opendds_cpp
 . install/local_setup.bash
 colcon build --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rcl_interfaces examples_rclcpp_minimal_publisher examples_rclcpp_minimal_subscriber $build_rpc
-
-popd &> /dev/null
-popd &> /dev/null
