0b9d8bdfc490e8407b09d3147e86e23aceae15e4
Jeremy Adams <adamsj@objectcomputing.com>
2020-06-24 17:42:42 -0500

    cleanup run_system_test.sh

--- run_system_test.sh
+++ run_system_test.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
-alt_rmw="rmw_opendds_cpp"
-# run_test="examples_rclcpp_minimal_publisher publisher_member_function"
-gdb_cmd="gdb -ex run --args"
-# instrumented=""
 
-# while getopts ":ha:sbi" opt; do
-while getopts ":ha:b" opt; do
+alt_rmw="rmw_opendds_cpp"
+
+while getopts ":ha:" opt; do
 case ${opt} in
     a )
         alt_rmw=$OPTARG
         echo "NOTE: For optional DDS implementations, only rmw_fastrtps_cpp is installed by default. All others require extra installation steps. https://index.ros.org/doc/ros2/Installation/DDS-Implementations/."
     ;;
-    # s )
-    #     run_test="examples_rclcpp_minimal_subscriber subscriber_member_function"
-    # ;;
-    b )
-        gdb_cmd="gdb --args"
-    ;;
-    # i )
-    #     instrumented="_instrumented"
-    # ;;
-    h ) echo "options: [-a] use alternate rmw (i.e. rmw_cyclonedds_cpp, rmw_fastrtps_cpp, or rmw_connext_cpp). [-b] breakpoint debugging"
+    h ) echo "options: [-a] use alternate rmw (i.e. rmw_cyclonedds_cpp, rmw_fastrtps_cpp, or rmw_connext_cpp)."
     exit
     ;;
 esac
@@ -30,11 +18,11 @@ script=`realpath $0`
 script_path=`dirname $script`
 pushd $script_path &> /dev/null
 pushd .. &> /dev/null
+
 . /opt/ros/foxy/setup.bash
 . install/local_setup.bash
 
-# eval RMW_IMPLEMENTATION="$alt_rmw ros2 run --prefix '${gdb_cmd}' $run_test$instrumented"
-RMW_IMPLEMENTATION=$alt_rmw
+export RMW_IMPLEMENTATION=$alt_rmw
 colcon test --packages-up-to test_communication
 colcon test-result --all
 
