3efc0472f167a4af3b6a34decfaf2298c7d3a80a
Jeremy Adams <adamsj@objectcomputing.com>
2020-06-23 21:20:56 -0500

    clean up rpc test

--- run_ci_test_rpc.sh
+++ run_ci_test_rpc.sh
@@ -9,8 +9,9 @@ printenv|grep ROS
 export RMW_IMPLEMENTATION=rmw_opendds_cpp
 printenv|grep RMW
 
+rm client.out
 ros2 run examples_rclcpp_minimal_service service_main &
-stdbuf -o0 ros2 run examples_rclcpp_minimal_client client_main > client.out
+stdbuf -o0 ros2 run examples_rclcpp_minimal_client client_main &> client.out
 
 pkill service_main
 pkill client_main
