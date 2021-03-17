98dfcbd7294c4aeea2a2223bd11d6109135df502
Jeremy Adams <adamsj@objectcomputing.com>
2021-03-17 14:42:17 -0500

    have to source opendds's setenv.sh
    not sure how this would work on ROS build farm

--- build_all.sh
+++ build_all.sh
@@ -5,5 +5,6 @@ export PATH=/usr/lib/ccache:$PATH
 
 echo "BUILD ALL"
 ./rmw_build/build_rmw.sh
+. src/opendds/setenv.sh
 . install/local_setup.bash
 colcon build --symlink-install --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rcl_interfaces examples_rclcpp_minimal_publisher examples_rclcpp_minimal_subscriber examples_rclcpp_minimal_service examples_rclcpp_minimal_client
