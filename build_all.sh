e1cb8fdad3f5c1384063ee7a0cc1446bb8085a20
Jeremy Adams <adamsj@objectcomputing.com>
2021-03-22 16:03:44 -0500

    try abs path for *_ROOT vars

--- build_all.sh
+++ build_all.sh
@@ -5,10 +5,10 @@ export PATH=/usr/lib/ccache:$PATH
 
 echo "BUILD ALL"
 ./rmw_build/build_opendds.sh
-export ACE_ROOT=install/opendds/share/ace
-export TAO_ROOT=install/opendds/share/tao
-export DDS_ROOT=install/opendds/share/dds
-export MPC_ROOT=install/opendds/share/mpc
+export ACE_ROOT=/opt/workspace/install/opendds/share/ace
+export TAO_ROOT=/opt/workspace/install/opendds/share/tao
+export DDS_ROOT=/opt/workspace/install/opendds/share/dds
+export MPC_ROOT=/opt/workspace/install/opendds/share/mpc
 ./rmw_build/build_rmw.sh
 . install/local_setup.bash
 colcon build --symlink-install --cmake-args '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON' '-DCMAKE_BUILD_TYPE=Debug' --packages-up-to rcl_interfaces examples_rclcpp_minimal_publisher examples_rclcpp_minimal_subscriber examples_rclcpp_minimal_service examples_rclcpp_minimal_client
